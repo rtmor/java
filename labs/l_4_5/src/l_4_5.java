7da09e6c5ccb7e1d162f3a1f5983bd30fb368ba9
Ryan T. Moran <rt.moran0@gmail.com>
2019-09-26 18:20:07 -0400

    added l_4_5

---
+++ labs/l_4_5/src/l_4_5.java
@@ -0,0 +1,11 @@
+/**
+ * L.4.5: Using the attached code as a basis, compute the Power Set of a set of
+ * numbers. You must use the attached code as a basis and you must use
+ * ArrayLists throughout as your data structure.
+ */
+public class l_4_5 {
+
+    public static void main(String[] args) {
+
+    }
+}
